7970732b5acf31eaad648d3a91bf1d4baed36204
眞貝拓未 <takumi@makaitakuhitsujis-MacBook-Pro.local>
2022-08-04 17:19:10 +0900

    command?

--- app.rb
+++ app.rb
@@ -5,11 +5,15 @@ require 'dotenv'
 Dotenv.load ".env"
 
 def client
-@client ||= Line::Bot::Client.new { |config|
-    config.channel_id = ENV["LINE_CHANNEL_ID"]
-    config.channel_secret = ENV["LINE_CHANNEL_SECRET"]
-    config.channel_token = ENV["LINE_CHANNEL_TOKEN"]
-}
+    @client ||= Line::Bot::Client.new { |config|
+        config.channel_id = ENV["LINE_CHANNEL_ID"]
+        config.channel_secret = ENV["LINE_CHANNEL_SECRET"]
+        config.channel_token = ENV["LINE_CHANNEL_TOKEN"]
+    }
+end
+
+def command?(message)
+    message.str[0] == 'ココ'
 end
 
 post '/callback' do
@@ -18,17 +22,18 @@ post '/callback' do
     unless client.validate_signature(body, signature)
         error 400 do 'Bad Request' end
     end
+
     events = client.parse_events_from(body)
     events.each do |event|
-        if event.is_a?(Line::Bot::Event::Message)
-        if event.type === Line::Bot::Event::MessageType::Text
-            message = {
+        unless event.is_a?(Line::Bot::Event::Message) return "not message"
+        unless event.type === Line::Bot::Event::MessageType::Text return "not text"
+        unless command?(event.message) return "not command"
+
+        message = {
             type: 'text',
             text: event.message['text']
-            }
-            client.reply_message(event['replyToken'], message)
-        end
-        end
+        }
+        client.reply_message(event['replyToken'], message)
     end
     "OK"
 end
