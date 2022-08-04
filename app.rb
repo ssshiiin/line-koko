51e0c73dc13ea5a3918c9a19cf71366ffdf089a1
眞貝拓未 <takumi@makaitakuhitsujis-MacBook-Pro.local>
2022-08-04 17:06:05 +0900

    greeting

--- app.rb
+++ app.rb
@@ -14,24 +14,23 @@ end
 
 post '/callback' do
     body = request.body.read
-    p body
-    # signature = request.env['HTTP_X_LINE_SIGNATURE']
-    # unless client.validate_signature(body, signature)
-    #     error 400 do 'Bad Request' end
-    # end
-    # events = client.parse_events_from(body)
-    # events.each do |event|
-    #     if event.is_a?(Line::Bot::Event::Message)
-    #     if event.type === Line::Bot::Event::MessageType::Text
-    #         message = {
-    #         type: 'text',
-    #         text: event.message['text']
-    #         }
-    #         client.reply_message(event['replyToken'], message)
-    #     end
-    #     end
-    # end
-    # "OK"
+    signature = request.env['HTTP_X_LINE_SIGNATURE']
+    unless client.validate_signature(body, signature)
+        error 400 do 'Bad Request' end
+    end
+    events = client.parse_events_from(body)
+    events.each do |event|
+        if event.is_a?(Line::Bot::Event::Message)
+        if event.type === Line::Bot::Event::MessageType::Text
+            message = {
+            type: 'text',
+            text: event.message['text']
+            }
+            client.reply_message(event['replyToken'], message)
+        end
+        end
+    end
+    "OK"
 end
 # ====== 追記ここまで ======
 get '/' do
