47a9ab9b77f27a267899adcedb3997cd040e58eb
眞貝拓未 <takumi@makaitakuhitsujis-MacBook-Pro.local>
2022-08-04 16:50:12 +0900

    test

--- Gemfile
+++ Gemfile
@@ -3,7 +3,6 @@
 source "https://rubygems.org"
 
 ruby "2.6.10"
-gem 'puma', '~> 3.7'
 gem 'line-bot-api'
 gem 'sinatra'
 gem 'dotenv'
--- config/puma.rb
+++
@@ -1,8 +0,0 @@
-workers ENV.fetch("WEB_CONCURRENCY") { 2 }
-
-preload_app!
-
-
-on_worker_boot do
-  ActiveRecord::Base.establish_connection if defined?(ActiveRecord)
-end
